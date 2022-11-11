dde525c431d5dbd8e9bcc2c90a995c76487df5b1
RUSANOVSCHI <rusanovschi1994@gmail.com>
2022-11-11 11:26:35 +0200

    work with ImplicitWait() and ExplicitWait()

--- selenium/src/main/java/DropDown.java
+++ selenium/src/main/java/DropDown.java
@@ -2,12 +2,15 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 
 public class DropDown {
 
-    private static WebDriver driver;
+    static WebDriver driver;
+    static WebDriverWait driverWait;
 
     public static void main(String[] args) {
 
@@ -18,12 +21,16 @@ public class DropDown {
 
         driver.findElement(By.xpath("//a[@role=\"button\" and text()=\"Create new account\"]")).click();
 
+        driverWait = (new WebDriverWait(driver, Duration.ofSeconds(5)));
+        driverWait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[@class=\"mbs _52lq fsl fwb fcb\"]")));
+        driverWait.until(ExpectedConditions.titleContains("Facebook"));
+
         //complete registration data
         completeRegistrationData("First name", "Vitalie");
         completeRegistrationData("Last name", "Tiltu");
-        completeRegistrationData("email", "tiltu@gmail.com");
-        completeRegistrationData("Re-enter email", "tiltu@gmail.com");
-        completeRegistrationData("password", "1111");
+        completeRegistrationData("email", "rusanovschi1994@gmail.com");
+        completeRegistrationData("Re-enter email", "rusanovschi1994@gmail.com");
+        completeRegistrationData("password", "password1111");
 
 
         //select Birthday
@@ -32,8 +39,9 @@ public class DropDown {
         selectOption("Year", "1994");
 
         //select gender
-
         selectGenderButton("Male");
+
+        driver.findElement(By.xpath("//div[@class=\"_1lch\"]/button[text()=\"Sign Up\"]")).click();
     }
 
     public static void setDriver(String url){
@@ -41,29 +49,28 @@ public class DropDown {
         String nameUrl = url;
         driver.get(nameUrl);
         driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+        //Implicit wait
+        //driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
     }
 
     public static void selectOption(String listName, String option){
-
-        String listXpath = String.format("//span/select[@title=\"%s\"]", listName);
+        String listXpath = String.format("//span[@class=\"_5k_4\"]/span/select[@title=\"%s\"]", listName);
         String optionXpath = String.format(listXpath + "/option[text()=\"%s\"]", option);
 
         driver.findElement(By.xpath(listXpath)).click();
+        driverWait
+                .until(ExpectedConditions
+                        .visibilityOfAllElementsLocatedBy(By.xpath(String.format("//select[@title=\"%s\"]/option", listName))));
         driver.findElement(By.xpath(optionXpath)).click();
-        driver.findElement(By.xpath(listXpath)).click();
     }
 
     public static void completeRegistrationData(String field, String writeData){
 
         String fieldXpath = String.format("//input[@class=\"inputtext _58mg _5dba _2ph-\" and contains(@aria-label, '%s')]", field);
 
-        try {
-            driver.findElement(By.xpath(fieldXpath)).sendKeys(writeData);
-            Thread.sleep(500);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        driverWait
+                .until(ExpectedConditions.visibilityOfElementLocated(By.xpath(fieldXpath)));
+        driver.findElement(By.xpath(fieldXpath)).sendKeys(writeData);
     }
 
     public static void selectGenderButton(String gender){
