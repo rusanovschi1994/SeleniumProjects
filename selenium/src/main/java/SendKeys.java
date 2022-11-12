cb7ba6330df381185789e715e71a07798796baf9
rusanovschi1994 <rusanovschi1994@gmail.com>
2022-11-12 20:01:09 +0200

    worked with Keys.chord() method

--- selenium/src/main/java/SendKeys.java
+++ selenium/src/main/java/SendKeys.java
@@ -4,26 +4,39 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.security.Key;
 import java.time.Duration;
 
 public class SendKeys {
 
-    public static void main(String[] args) {
+    static WebDriver driver;
+
+    public static void main(String[] args) throws InterruptedException {
+
+        getChromeDriver("http://wiki.microinvest.su/");
+
+        WebElement searchInput = driver.findElement(By.xpath("//input[@id=\"searchInput\"]"));
+        String select = Keys.chord(Keys.CONTROL + "a");
+        String cut = Keys.chord(Keys.CONTROL + "x");
+        String paste = Keys.chord(Keys.CONTROL + "v");
+
+        searchInput.sendKeys("noLogo");
+        searchInput.sendKeys(select);
+        Thread.sleep(2000);
+        searchInput.sendKeys(cut);
+        Thread.sleep(3000);
+        searchInput.sendKeys(paste, Keys.ENTER);
+        Thread.sleep(4000);
+
+        driver.quit();
+    }
+
+    public static void getChromeDriver(String url){
 
         System.setProperty("webdriver.chrome.driver", "C:\\drivers\\selenium\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-
+        driver = new ChromeDriver();
+        driver.get(url);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-
-        driver.get("https://www.udemy.com/");
-
-        WebElement inputElement = driver.findElement(By.xpath("(//input[@type='text'])[1]"));
-        inputElement.click();
-        inputElement.sendKeys("Java", Keys.ENTER);
-
-
-        WebElement selectCourse = driver.findElement(By.xpath("//a[@href=\"/course/neutrino-java-foundations/\"]"));
-        selectCourse.click();
-
+        driver.manage().window().maximize();
     }
 }
