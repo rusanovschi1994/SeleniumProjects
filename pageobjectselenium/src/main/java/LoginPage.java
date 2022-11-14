9c8890371c3b2f5af0e3bf0bd79539e974e76980
rusanovschi1994 <rusanovschi1994@gmail.com>
2022-11-15 00:22:55 +0200

    added createAccount() method

--- pageobjectselenium/src/main/java/LoginPage.java
+++ pageobjectselenium/src/main/java/LoginPage.java
@@ -13,6 +13,9 @@ public class LoginPage {
     private By passwordField = By.xpath("//input[@id=\"password\"]");
     private By signInButton = By.xpath("//input[@type=\"submit\"]");
     private By heading = By.xpath("//div[contains(@class, 'auth-form-header')]/h1");
+    private By loginError = By.xpath("//div[@id=\"js-flash-container\"]//div[@role=\"alert\"]");
+    private By createAccountLink = By.xpath("//a[text()=\"Create an account\"]");
+
 
     public LoginPage typeUsername(String username){
         driver.findElement(usernameField).sendKeys(username);
@@ -33,5 +36,14 @@ public class LoginPage {
 
     public String getHeadingText(){
         return driver.findElement(heading).getText();
+    }
+
+    public String getLoginError(){
+        return driver.findElement(loginError).getText();
+    }
+
+    public SignUpPage createAccount(){
+        driver.findElement(createAccountLink).click();
+        return new SignUpPage(driver);
     }
 }
