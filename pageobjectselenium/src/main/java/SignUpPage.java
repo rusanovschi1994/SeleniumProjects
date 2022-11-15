93cb491e7779dc88dd1f4dbd6ab800ce9deca72e
RUSANOVSCHI <rusanovschi1994@gmail.com>
2022-11-15 10:31:13 +0200

    added locators for SignUpPage

--- pageobjectselenium/src/main/java/SignUpPage.java
+++ pageobjectselenium/src/main/java/SignUpPage.java
@@ -1,10 +1,17 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class SignUpPage {
 
-    WebDriver driver;
+    private WebDriver driver;
 
     public SignUpPage(WebDriver driver) {
         this.driver = driver;
     }
+
+    private By headingText = By.xpath("//div[contains(@class, 'm-4')]/h1");
+    private By emailField = By.xpath("//input[@id=\"email\"]");
+    private By continueButton = By.xpath("//div[contains(@class, 'd-flex flex-items-center flex-column')]/button");
+    private By passwordField = By.xpath("//input[@id=\"password\"]");
+
 }
